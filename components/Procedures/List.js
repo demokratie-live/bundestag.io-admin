923307bf83410682a8fd9f337000b2860be7e94d
Manuel Ruck <github.com@manuelruck.de>
2019-10-30 14:30:02 +0700

    clean up

--- components/Procedures/List.js
+++ components/Procedures/List.js
@@ -1,35 +1,20 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import {
-  List,
   Icon,
-  Spin,
-  Divider,
-  Checkbox,
   Table,
   Input,
   Button
 } from "antd";
 import Link from "next/link";
 
-import InfiniteScroll from "react-infinite-scroller";
-
 import PROCEDURE_LIST from "../../graphql/queries/procedureList";
 
 const PAGE_SIZE = 10000;
 
-const IconText = ({ type, text }) => (
-  <span style={{ filter: "blur(0px)" }}>
-    <Icon type={type} style={{ marginRight: 8 }} />
-    {text}
-  </span>
-);
-
 class ProcedureList extends Component {
-  static onlyWithoutVoteData = false;
   state = {
     hasMore: true,
-    onlyWithoutVoteData: ProcedureList.onlyWithoutVoteData,
     searchText: ""
   };
 
@@ -206,105 +191,16 @@ class ProcedureList extends Component {
     }
   ];
 
-  loadedRowsMap = {};
-
-  isRowLoaded = ({ index }) => {
-    return !!this.loadedRowsMap[index];
-  };
-
-  renderItem = ({
-    title,
-    procedureId,
-    namedVote,
-    customData: { voteResults, possibleVotingDate, expectedVotingDate },
-    currentStatus
-  }) => {
-    let icons = [];
-    if (
-      voteResults.yes > 0 ||
-      voteResults.no > 0 ||
-      voteResults.abstination > 0
-    ) {
-      icons.push(<Icon key={"pie-chart"} type="pie-chart" />);
-    }
-    if (namedVote) {
-      icons.push(<Icon key={"idcard"} type="idcard" />);
-    }
-    if (possibleVotingDate && !expectedVotingDate) {
-      icons.push(
-        <Icon
-          key={"thunderbolt"}
-          type="thunderbolt"
-          theme="twoTone"
-          twoToneColor={
-            possibleVotingDate === expectedVotingDate ? "green" : "#eb2f96"
-          }
-        />
-      );
-    }
-    icons = icons
-      .reduce(
-        (prev, icon, i) => [
-          ...prev,
-          icon,
-          <Divider key={`divider-${i}`} type="vertical" />
-        ],
-        []
-      )
-      .slice(0, -1);
-
-    if (
-      this.state.onlyWithoutVoteData &&
-      (voteResults.yes > 0 || voteResults.no > 0 || voteResults.abstination > 0)
-    ) {
-      return <></>;
-    }
-
-    return (
-      <List.Item key={procedureId}>
-        <List.Item.Meta
-          title={
-            <Link
-              as={`/procedure/${procedureId}`}
-              href={`/procedure?id=${procedureId}`}
-            >
-              <a>
-                {procedureId} - {currentStatus} – {title}
-              </a>
-            </Link>
-          }
-          description={<>{icons.map(icon => icon)}</>}
-        />
-      </List.Item>
-    );
-  };
-
   render() {
-    const { procedures, loadingProcedures } = this.props;
+    const { procedures } = this.props;
 
     return (
       <div>
-        <Checkbox
-          onChange={({ target: { checked } }) => {
-            this.setState({ onlyWithoutVoteData: checked });
-            ProcedureList.onlyWithoutVoteData = checked;
-          }}
-          checked={this.state.onlyWithoutVoteData}
-        >
-          ohne Abstimmungsdaten
-        </Checkbox>
         <Table
           columns={this.columns}
           dataSource={procedures}
           rowKey={procedure => procedure.procedureId}
         />
-        <List dataSource={procedures} renderItem={this.renderItem}>
-          {loadingProcedures && this.state.hasMore && (
-            <div className="demo-loading-container">
-              <Spin />
-            </div>
-          )}
-        </List>
       </div>
     );
   }
@@ -323,7 +219,7 @@ export default graphql(PROCEDURE_LIST, {
     notifyOnNetworkStatusChange: true,
     fetchPolicy: "cache-and-network"
   },
-  props: ({ data, data: { procedures, loading } }, props) => {
+  props: ({ data: { procedures, loading } }, props) => {
     return {
       procedures: procedures || [],
       loadingProcedures: loading
