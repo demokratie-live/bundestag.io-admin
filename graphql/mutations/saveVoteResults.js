cf931467dc317d7720463df38442229189068f50
ManAnRuck <github@manuelruck.de>
2020-07-17 17:06:18 +0200

    fix enum query
    Signed-off-by: ManAnRuck <github@manuelruck.de>

--- graphql/mutations/saveVoteResults.js
+++ graphql/mutations/saveVoteResults.js
@@ -3,11 +3,12 @@ import gql from "graphql-tag";
 import VoteResults from "../fragments/voteResults";
 
 export default gql`
+
   mutation saveProcedureCustomData(
     $procedureId: String!
     $partyVotes: [PartyVoteInput!]!
     $decisionText: String!
-    $votingDocument: String!
+    $votingDocument: VotingDocument!
   ) {
     saveProcedureCustomData(
       procedureId: $procedureId
